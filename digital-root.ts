3c8afe2860a021f5fc4a942af1a155345a629847
tlightk <klaysi@gmail.com>
2023-03-23 17:20:54 +0900

    added condition for single digit input

--- digital-root.ts
+++ digital-root.ts
@@ -1,3 +1,9 @@
 function digitalRoot(input: number):number {
+    let digits:string = input.toString();
+    if (digits.length === 1) {
+        return parseInt(digits);
+    }
     return 3;
 }
+
+console.log(digitalRoot(7));
