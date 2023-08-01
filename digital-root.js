d9690cedb2c47fa86ff982f1427bf7ca69db2c98
klay <klayxi@gmail.com>
2023-08-01 21:21:53 +0900

    add else branch, initiate create array function

--- digital-root.js
+++ digital-root.js
@@ -1,3 +1,16 @@
+"use strict";
 function digitalRoot(input) {
-    return 3;
+    let digits = input.toString();
+    if (digits.length === 1) {
+        return parseInt(digits);
+    }
+    else {
+        let digitArray = [];
+        for (let i = 0; i < digits.length; i++) {
+            digitArray.push(parseInt(digits[i]));
+        }
+        console.log(digitArray);
+        return 3;
+    }
 }
+console.log(digitalRoot(237));
--- digital-root.ts
+++ digital-root.ts
@@ -1,9 +1,22 @@
-function digitalRoot(input: number):number {
-    let digits:string = input.toString();
-    if (digits.length === 1) {
-        return parseInt(digits);
+function digitalRoot(input: number): number {
+  let digits: string = input.toString();
+  if (digits.length === 1) {
+    return parseInt(digits);
+  } else {
+    let digitArray: number[] = [];
+
+    // function createDigitArray()
+    for (let i: number = 0; i < digits.length; i++) {
+        digitArray.push(parseInt(digits[i]));
     }
+
+    while (digitArray.length !== 1) {
+
+    }
+
+    console.log(digitArray);
     return 3;
+  }
 }
 
-console.log(digitalRoot(7));
+console.log(digitalRoot(237));
